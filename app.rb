a03577aecb85989041ba9bf47a23ccfd8d1e577b
Matthew Downs <mwdowns@gmail.com>
2020-01-25 17:46:00 -0500

    move schema into correct dir

--- app.rb
+++ app.rb
@@ -5,11 +5,14 @@ require 'rack/contrib'
 
 # load models
 Dir['./models/*.rb'].each { |f| require_relative f }
+# load graphql
+require_relative './graphql/schema'
 
 Dotenv.load('./.env')
 
 class HappyPlaceApp < Sinatra::Base
   set :database_file, 'config/database.yml'
+  use Rack::PostBodyContentTypeParser
 
   get '/' do
     'It works!'
