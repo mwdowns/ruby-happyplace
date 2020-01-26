48101131a5b7729b76a91c4e9c8492e15c9f7555
Matthew Downs <mwdowns@gmail.com>
2020-01-25 21:23:56 -0500

    update mutation.rb and create_happy_place.rb

--- graphql/mutation.rb
+++ graphql/mutation.rb
@@ -1,8 +1,12 @@
 require 'graphql'
 require_relative './mutations/create_place'
+require_relative './mutations/create_user'
+require_relative './mutations/create_happy_place'
 
 class MutationType < GraphQL::Schema::Object
   description 'The mutation root of this schema'
 
   field :createPlace, mutation: Mutations::CreatePlace
+  field :createUser, mutation: Mutations::CreateUser
+  field :createHappyPlace, mutation: Mutations::CreateHappyPlace
 end
--- graphql/mutations/create_happy_place.rb
+++ graphql/mutations/create_happy_place.rb
@@ -4,19 +4,19 @@ require_relative 'base_mutation'
 class Mutations::CreateHappyPlace < Mutations::BaseMutation
   description 'create a new happy_place object'
 
-  argument :user, Types::User, required: true
-  argument :place, Types::Place, required: true
-  argument :message, String, required: false,
+  argument :user_id, ID, required: true
+  argument :place_id, ID, required: true
+  argument :message, String, required: false
   argument :media_url, String, required: false
 
   field :success, Boolean, null: false
   field :happy_place, Types::HappyPlace, null: true
   field :errors, String, null: false
 
-  def resolve(user:, place:, message: nil, media_url: nil)
+  def resolve(user_id:, place_id:, message: nil, media_url: nil)
     happy_place = HappyPlace.new(
-      user: user, 
-      place: place, 
+      user_id: user_id, 
+      place_id: place_id, 
       message: message, 
       media_url: media_url
     )
